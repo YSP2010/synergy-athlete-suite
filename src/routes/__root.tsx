7a6f35d4e0507ee21fd66cb6136c0211548773ba
YSP2010 <yspakdaman@gmail.com>
2026-08-06 13:14:14 +0200

    Remove unused LanguageProvider import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,7 +14,6 @@ import { reportLovableError } from "../lib/lovable-error-reporting";
 import { supabase } from "@/integrations/supabase/client";
 import { Toaster } from "@/components/ui/sonner";
 import { registerServiceWorker } from "@/lib/pwa/register";
-import { LanguageProvider } from "@/lib/i18n";
 
 function NotFoundComponent() {
   return (
